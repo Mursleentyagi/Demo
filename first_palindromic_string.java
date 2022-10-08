eb21bfa0ead14467e5b7322d7242772dc76229a6
Mursleentyagi <94542151+Mursleentyagi@users.noreply.github.com>
2022-10-08 08:45:47 +0530

    Add files via upload

---
+++ first_palindromic_string.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Stack;
+
+
+public class Debugger {
+    public static void main(String[] args) {
+       String[] words = {"notapalindrome","racecar"};
+//        Output:"ada"
+        System.out.println(first_palindrome(words));
+    }
+    static String first_palindrome(String[] words){
+        boolean ans=false;
+        for(int i=0;i< words.length;i++){
+            if(is_palindrome(words[i])){
+                ans=true;
+                return words[i];
+            }
+        }
+        return null;
+    }
+    static  boolean is_palindrome(String s){
+        int i=0;
+        int j=s.length()-1;
+        boolean ans=true;
+        while(i<j){
+            if(s.charAt(i)==s.charAt(j)){
+                ans=true;
+            }
+            else{
+                ans=false;
+                break;
+            }
+            i++;
+            j--;
+        }
+       return ans;
+    }
+}
